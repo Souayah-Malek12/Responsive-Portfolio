01510347b9be9bff97e34d4e2420c494401c7bd6
Souayah-Malek12 <malek.souayah@isimg.tn>
2024-08-25 23:38:59 +0100

    Starting working on Responsive portfolio

---
+++ app.js
@@ -0,0 +1,8 @@
+const x = document.querySelector("#x")
+const menu = document.querySelector('#menu')
+
+x.addEventListener("click", ()=> {
+    menu.classList.toggle("hidden")
+    x.classList.toggle("bg-white ")
+    })
+console.log('sgqgf')
